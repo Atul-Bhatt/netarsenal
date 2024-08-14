641b33d526123f85210f17437eb37ed806328daa
Atul Bhatt <atulbhatt61@gmail.com>
2024-08-14 22:32:08 +0530

    portscanner: socket

--- cmd/portscanner/main.go
+++ cmd/portscanner/main.go
@@ -1,9 +1,36 @@
 package main
 
 import(
+	"net"
 	"fmt"
+	"flag"
 )
 
+type Flags struct {
+	ipAdd string
+}
+
+func (f *Flags) commandLineFlags() {
+	flag.StringVar(&f.ipAdd, "ip", "", "Provide IP address")
+	flag.Parse()
+}
+
 func main() {
-	fmt.Println("This is a port scanner")
+	var cliArgs Flags
+	cliArgs.commandLineFlags()
+
+	conn, err := net.Dial("tcp", cliArgs.ipAdd)
+	if err != nil {
+		fmt.Println("Failed to create a connection")
+		return
+	}
+
+	var b []byte
+	_, readErr := conn.Read(b)
+	if readErr != nil {
+		fmt.Println("Failed to read from connection")
+		return
+	}
+
+	fmt.Println(b)
 }

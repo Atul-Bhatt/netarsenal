d66852842dbbbf8e69ae5601f7e63f7c9ca495ea
Atul Bhatt <atulbhatt61@gmail.com>
2024-08-15 21:56:08 +0530

    deadline for connection timeout

--- cmd/portscanner/main.go
+++ cmd/portscanner/main.go
@@ -4,7 +4,10 @@ import(
 	"net"
 	"fmt"
 	"flag"
+	"time"
 )
+
+const PORT = "80"
 
 type Flags struct {
 	ipAdd string
@@ -19,18 +22,24 @@ func main() {
 	var cliArgs Flags
 	cliArgs.commandLineFlags()
 
-	conn, err := net.Dial("tcp", cliArgs.ipAdd)
+	conn, err := net.Dial("tcp", cliArgs.ipAdd + ":" +  PORT)
 	if err != nil {
 		fmt.Println("Failed to create a connection")
 		return
 	}
+	defer conn.Close()
 
-	var b []byte
-	_, readErr := conn.Read(b)
+	fmt.Println("Scanning ports for " + cliArgs.ipAdd)
+
+	// set a read deadline
+	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+	b := make([]byte, 1024)
+	readFromConn, readErr := conn.Read(b)
 	if readErr != nil {
 		fmt.Println("Failed to read from connection")
 		return
 	}
 
-	fmt.Println(b)
+	fmt.Println(b, readFromConn)
 }
